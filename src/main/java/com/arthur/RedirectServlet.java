b291522a8fcd3ef17103941b753bf01af21ab1c5
Cyitatire Uwonizeye Arthur <cyitatire15arthur@gmail.com>
2026-02-04 21:14:55 +0200

    first assignment commit

--- src/main/java/com/arthur/RedirectServlet.java
+++ src/main/java/com/arthur/RedirectServlet.java
@@ -1,5 +1,4 @@
 package com.arthur;
-
 import java.io.IOException;
 
 import jakarta.servlet.ServletException;
@@ -15,15 +14,10 @@ public class RedirectServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException {
         
-        // 1. Grab the search term from the URL (e.g., ?query=apples)
         String userQuery = request.getParameter("query");
-
-        // 2. The Redirect Magic
-        // We take Google's search URL and append the user's word to the end
         if (userQuery != null && !userQuery.trim().isEmpty()) {
             response.sendRedirect("https://www.google.com/search?q=" + userQuery);
         } else {
-            // If they typed nothing, just go to Google home
             response.sendRedirect("https://www.google.com");
         }
     }
